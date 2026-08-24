cf36eb6f8bcb892a99b3dbcf81fc9514c2ec4fe8
roshan pawar <roshanpawar48640@gmail.com>
2026-08-24 23:33:57 +0530

    adding css module for folders specific

---
+++ 05-css/src/components/button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../button/Button.module.css'
+
+const button = () => {
+  return (
+    <div className={styles.btn}>
+      <button>click me</button>
+    </div>
+  )
+}
+
+export default button
