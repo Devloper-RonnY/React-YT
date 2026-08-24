f61b38acd356c0d58f70d6ef16b043d61c5a152b
roshan pawar <roshanpawar48640@gmail.com>
2026-08-24 22:16:10 +0530

    understanding props and react structure

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -1,18 +1,14 @@
 import Card from './components/Card'
-import {
-    faAmazon,
-    faMicrosoft,
-    faGoogle,
-    faApple
-} from "@fortawesome/free-brands-svg-icons";
+import jobs from './data/jobs'
 
 const App = () => {
   return (
-    <div className='parent'>
-      <Card logo={faAmazon} company="Amazon" time="5 days ago" job="frontend devloper" salary={100} location="Mumbai, India"/>
-      <Card logo={faApple} company="Apple" time="1 hour ago" job="Backend Devloper" salary={100} location="Banglore, India"/>
-      <Card logo={faMicrosoft} company="Microsoft" time="just now" job="Full Stack Devloper" salary={120} location="Chennai, India"/>
-      <Card logo={faGoogle} company="Google" time="2 days ago" job="AI Devloper" salary={220} location="Hydrabad, India"/>
+    <div className='parent'> 
+        {jobs.map((job,idx) => {
+       return <div key={idx}>
+         <Card logo={job.logo} company={job.company} time={job.postedAgo} job={job.jobTitle} salary={job.salary} location={job.location}/>
+       </div>
+        })}
     </div>
   )
 }
---
+++ 04-cards-project/src/data/jobs.jsx
@@ -0,0 +1,86 @@
+import { faAmazon, faApple, faMicrosoft, faGoogle, faMeta, faLinkedin, faGithub, faSpotify, faDiscord, faSlack } from "@fortawesome/free-brands-svg-icons";
+
+const jobs = [
+    {
+        logo: faAmazon,
+        company: "Amazon",
+        postedAgo: "5 days ago",
+        jobTitle: "Frontend Developer",
+        salary: 100,
+        location: "Mumbai, India"
+    },
+    {
+        logo: faApple,
+        company: "Apple",
+        postedAgo: "1 hour ago",
+        jobTitle: "Backend Developer",
+        salary: 120,
+        location: "Bangalore, India"
+    },
+    {
+        logo: faMicrosoft,
+        company: "Microsoft",
+        postedAgo: "Just now",
+        jobTitle: "Full Stack Developer",
+        salary: 150,
+        location: "Hyderabad, India"
+    },
+    {
+        logo: faGoogle,
+        company: "Google",
+        postedAgo: "2 days ago",
+        jobTitle: "Software Engineer",
+        salary: 200,
+        location: "Bangalore, India"
+    },
+    {
+        logo: faMeta,
+        company: "Meta",
+        postedAgo: "3 hours ago",
+        jobTitle: "React Developer",
+        salary: 180,
+        location: "Mumbai, India"
+    },
+    {
+        logo: faLinkedin,
+        company: "LinkedIn",
+        postedAgo: "1 day ago",
+        jobTitle: "Frontend Engineer",
+        salary: 140,
+        location: "Remote"
+    },
+    {
+        logo: faGithub,
+        company: "GitHub",
+        postedAgo: "4 days ago",
+        jobTitle: "Software Engineer",
+        salary: 160,
+        location: "Remote"
+    },
+    {
+        logo: faSpotify,
+        company: "Spotify",
+        postedAgo: "6 hours ago",
+        jobTitle: "Web Developer",
+        salary: 130,
+        location: "Mumbai, India"
+    },
+    {
+        logo: faDiscord,
+        company: "Discord",
+        postedAgo: "2 days ago",
+        jobTitle: "Frontend Developer",
+        salary: 150,
+        location: "Remote"
+    },
+    {
+        logo: faSlack,
+        company: "Slack",
+        postedAgo: "5 hours ago",
+        jobTitle: "React Developer",
+        salary: 170,
+        location: "Bangalore, India"
+    }
+];
+
+export default jobs;
